9a86f340524f235ba27266ee179fd63f8389f41d
Lolante <58864722+Lolante@users.noreply.github.com>
2024-02-12 12:31:16 +0200

    boad.tscn added

--- main.gd
+++ main.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
-
+##quit button
 func _on_button_pressed():
 	get_tree().quit()
 	
-
-
+##deck button
 func _on_button_2_pressed():
 	pass # Replace with function body.
+
+##dev-test button
+func _on_button_4_pressed():
+	get_tree().change_scene_to_file("res://board.tscn")
