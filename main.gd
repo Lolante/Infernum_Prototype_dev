a8b2ad3de1c331a7f7a8ef4301d4a74728ca9700
Lolante <58864722+Lolante@users.noreply.github.com>
2024-02-13 17:11:48 +0200

    more comment

--- main.gd
+++ main.gd
@@ -1,8 +1,6 @@
 extends Node2D
 
-var is_draggible = false
-
-##quit button
+##quit button  quits by closing the tree
 func _on_button_pressed():
 	get_tree().quit()
 	
@@ -10,6 +8,7 @@ func _on_button_pressed():
 func _on_button_2_pressed():
 	pass # Replace with function body.
 
-##dev-test button
+##dev-test button   returns to play area by loading board scene
 func _on_button_4_pressed():
 	get_tree().change_scene_to_file("res://board.tscn")
+
